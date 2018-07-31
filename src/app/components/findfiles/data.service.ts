da0f5092f7cfcbb375dd82974fa7cd47ac827e05
ricardomartinez9568 <rm3@mscpsharks.org>
2018-07-30 17:51:38 -0700

    heading home now 7/30/2018

--- src/app/components/findfiles/data.service.ts
+++ src/app/components/findfiles/data.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
-//import { Http, Headers, RequestOptions } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class DataService{
+export class DataService {
 
     constructor(private http: HttpClient) { }
 
-    baseUrl = "http://localhost:3000";
+    baseUrl = 'http://localhost:3000';
 
-    convertDocx(docx){
-        return this.http.post(this.baseUrl + "/convertDocx-Pdf", docx)
+    convertDocx(docx) {
+        return this.http.post(this.baseUrl + '/convertDocx-Pdf', docx);
     }
 
-    downloadPdf(){
-        return this.http.get(this.baseUrl+ '/downloadPdf')
+    downloadPdf() {
+        return this.http.get(this.baseUrl + '/downloadPdf');
     }
 }
